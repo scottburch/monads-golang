15df9c3e57d4518cfaf719c4c11b898b30453720
Scott Burch <scott@bulldoginfo.com>
2021-08-18 05:10:01 -0700

    cleaned up adapter types

--- adapters.go
+++ adapters.go
@@ -1,18 +1,22 @@
 package monads
 
-func IntToIntFn(fn func(x int) int) func(x interface{}) interface{} {
+type MapFn = func(x interface{}) interface{}
+type MapErrorFn = func(x interface{}) (interface{}, error)
+type BindFn = func(x interface{}) Validator
+
+func IntToIntFn(fn func(x int) int) MapFn {
 	return func(x interface{}) interface{} {
 		return fn(x.(int))
 	}
 }
 
-func IntToIntErrorFn(fn func(x int) (int, error)) func(x interface{}) (interface{}, error) {
+func IntToIntErrorFn(fn func(x int) (int, error)) MapErrorFn {
 	return func(x interface{}) (interface{}, error) {
 		return fn(x.(int))
 	}
 }
 
-func IntToValidatorFn(fn func(x int) Validator) func(x interface{}) Validator {
+func IntToValidatorFn(fn func(x int) Validator) BindFn {
 	return func(x interface{}) Validator {
 		return fn(x.(int))
 	}
